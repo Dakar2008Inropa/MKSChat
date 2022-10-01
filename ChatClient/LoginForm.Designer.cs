76837bcb1348da657688819a0b455cfefcc1b36a
Daniel Vinther Andersen <dva@inropa.com>
2022-10-02 00:53:37 +0200

    Prettyfied Chat Form

--- ChatClient/LoginForm.Designer.cs
+++ ChatClient/LoginForm.Designer.cs
@@ -28,6 +28,7 @@
     /// </summary>
     private void InitializeComponent()
     {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
       this.lblUsername = new System.Windows.Forms.Label();
       this.tbName = new System.Windows.Forms.TextBox();
       this.btnLogin = new System.Windows.Forms.Button();
@@ -35,27 +36,31 @@
       // 
       // lblUsername
       // 
-      this.lblUsername.AutoSize = true;
-      this.lblUsername.Location = new System.Drawing.Point(12, 16);
+      this.lblUsername.Location = new System.Drawing.Point(5, 5);
+      this.lblUsername.Margin = new System.Windows.Forms.Padding(0, 0, 0, 5);
       this.lblUsername.Name = "lblUsername";
-      this.lblUsername.Size = new System.Drawing.Size(60, 15);
+      this.lblUsername.Size = new System.Drawing.Size(382, 22);
       this.lblUsername.TabIndex = 0;
       this.lblUsername.Text = "&Username";
+      this.lblUsername.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
       // 
       // tbName
       // 
-      this.tbName.Location = new System.Drawing.Point(12, 34);
+      this.tbName.Location = new System.Drawing.Point(5, 32);
+      this.tbName.Margin = new System.Windows.Forms.Padding(0, 0, 0, 5);
       this.tbName.Name = "tbName";
-      this.tbName.Size = new System.Drawing.Size(238, 23);
+      this.tbName.Size = new System.Drawing.Size(382, 23);
       this.tbName.TabIndex = 1;
+      this.tbName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
       this.tbName.TextChanged += new System.EventHandler(this.tbName_TextChanged);
       // 
       // btnLogin
       // 
       this.btnLogin.DialogResult = System.Windows.Forms.DialogResult.OK;
-      this.btnLogin.Location = new System.Drawing.Point(256, 34);
+      this.btnLogin.Location = new System.Drawing.Point(5, 60);
+      this.btnLogin.Margin = new System.Windows.Forms.Padding(0);
       this.btnLogin.Name = "btnLogin";
-      this.btnLogin.Size = new System.Drawing.Size(75, 23);
+      this.btnLogin.Size = new System.Drawing.Size(382, 25);
       this.btnLogin.TabIndex = 2;
       this.btnLogin.Text = "&Ok";
       this.btnLogin.UseVisualStyleBackColor = true;
@@ -63,13 +68,20 @@
       // LoginForm
       // 
       this.AcceptButton = this.btnLogin;
-      this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(343, 75);
+      this.ClientSize = new System.Drawing.Size(392, 91);
       this.Controls.Add(this.btnLogin);
       this.Controls.Add(this.tbName);
       this.Controls.Add(this.lblUsername);
+      this.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+      this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+      this.MaximizeBox = false;
+      this.MinimizeBox = false;
       this.Name = "LoginForm";
+      this.Padding = new System.Windows.Forms.Padding(5);
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
       this.Text = "Login";
       this.ResumeLayout(false);
       this.PerformLayout();
--- ChatClient/MainForm.Designer.cs
+++ ChatClient/MainForm.Designer.cs
@@ -28,16 +28,20 @@
     /// </summary>
     private void InitializeComponent()
     {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
       this.btnLogin = new System.Windows.Forms.Button();
       this.tbInput = new System.Windows.Forms.TextBox();
       this.tbChatbox = new System.Windows.Forms.TextBox();
+      this.connectedLabel = new System.Windows.Forms.Label();
       this.SuspendLayout();
       // 
       // btnLogin
       // 
-      this.btnLogin.Location = new System.Drawing.Point(12, 12);
+      this.btnLogin.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+      this.btnLogin.Location = new System.Drawing.Point(8, 5);
+      this.btnLogin.Margin = new System.Windows.Forms.Padding(0);
       this.btnLogin.Name = "btnLogin";
-      this.btnLogin.Size = new System.Drawing.Size(75, 23);
+      this.btnLogin.Size = new System.Drawing.Size(784, 34);
       this.btnLogin.TabIndex = 0;
       this.btnLogin.Text = "Login";
       this.btnLogin.UseVisualStyleBackColor = true;
@@ -46,30 +50,51 @@
       // tbInput
       // 
       this.tbInput.AcceptsReturn = true;
-      this.tbInput.Location = new System.Drawing.Point(12, 415);
+      this.tbInput.Enabled = false;
+      this.tbInput.Location = new System.Drawing.Point(8, 415);
       this.tbInput.Name = "tbInput";
-      this.tbInput.Size = new System.Drawing.Size(776, 23);
+      this.tbInput.Size = new System.Drawing.Size(784, 23);
       this.tbInput.TabIndex = 1;
+      this.tbInput.Text = "You must login to start chatting...";
+      this.tbInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
       this.tbInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbInput_KeyDown);
       // 
       // tbChatbox
       // 
-      this.tbChatbox.Location = new System.Drawing.Point(12, 41);
+      this.tbChatbox.Location = new System.Drawing.Point(8, 45);
       this.tbChatbox.Multiline = true;
       this.tbChatbox.Name = "tbChatbox";
-      this.tbChatbox.Size = new System.Drawing.Size(776, 368);
+      this.tbChatbox.Size = new System.Drawing.Size(784, 364);
       this.tbChatbox.TabIndex = 2;
+      // 
+      // connectedLabel
+      // 
+      this.connectedLabel.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+      this.connectedLabel.ForeColor = System.Drawing.Color.Green;
+      this.connectedLabel.Location = new System.Drawing.Point(8, 5);
+      this.connectedLabel.Name = "connectedLabel";
+      this.connectedLabel.Size = new System.Drawing.Size(784, 34);
+      this.connectedLabel.TabIndex = 3;
+      this.connectedLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+      this.connectedLabel.Visible = false;
       // 
       // MainForm
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.ClientSize = new System.Drawing.Size(800, 445);
       this.Controls.Add(this.tbChatbox);
       this.Controls.Add(this.tbInput);
       this.Controls.Add(this.btnLogin);
+      this.Controls.Add(this.connectedLabel);
+      this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+      this.MaximizeBox = false;
+      this.MinimizeBox = false;
       this.Name = "MainForm";
-      this.Text = "MainForm";
+      this.Padding = new System.Windows.Forms.Padding(5);
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+      this.Text = "Chat";
+      this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
       this.ResumeLayout(false);
       this.PerformLayout();
 
@@ -80,5 +105,6 @@
         private Button btnLogin;
     private TextBox tbInput;
     private TextBox tbChatbox;
-  }
+        private Label connectedLabel;
+    }
 }
